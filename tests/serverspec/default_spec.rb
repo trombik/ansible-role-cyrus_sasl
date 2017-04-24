37911ba2ea9be07a7c2edf2fdfcaf2c471a6ba74
Tomoyuki Sakurai <tomoyukis@reallyenglish.com>
2017-04-24 13:58:58 +0900

    initial implementation

--- tests/serverspec/default_spec.rb
+++ tests/serverspec/default_spec.rb
@@ -1,58 +1,31 @@
-require 'spec_helper'
-require 'serverspec'
+require "spec_helper"
+require "serverspec"
 
-package = 'cyrus-sasl'
-service = 'cyrus-sasl'
-config  = '/etc/cyrus-sasl/cyrus-sasl.conf'
-user    = 'cyrus-sasl'
-group   = 'cyrus-sasl'
-ports   = [ PORTS ]
-log_dir = '/var/log/cyrus-sasl'
-db_dir  = '/var/lib/cyrus-sasl'
+package = "cyrus-sasl"
+sasldblistusers_command = "sasldblistusers2"
+sasldb_file = "/etc/sasldb2.db"
+default_user = "root"
+default_group = "root"
 
 case os[:family]
-when 'freebsd'
-  config = '/usr/local/etc/cyrus-sasl.conf'
-  db_dir = '/var/db/cyrus-sasl'
+when "freebsd"
+  sasldb_file = "/usr/local/etc/sasldb2.db"
+  default_group = "wheel"
 end
 
 describe package(package) do
   it { should be_installed }
 end 
 
-describe file(config) do
+describe file(sasldb_file) do
   it { should be_file }
-  its(:content) { should match Regexp.escape('cyrus-sasl') }
+  it { should be_mode 600 }
+  it { should be_owned_by default_user }
+  it { should be_grouped_into default_group }
 end
 
-describe file(log_dir) do
-  it { should exist }
-  it { should be_mode 755 }
-  it { should be_owned_by user }
-  it { should be_grouped_into group }
-end
-
-describe file(db_dir) do
-  it { should exist }
-  it { should be_mode 755 }
-  it { should be_owned_by user }
-  it { should be_grouped_into group }
-end
-
-case os[:family]
-when 'freebsd'
-  describe file('/etc/rc.conf.d/cyrus-sasl') do
-    it { should be_file }
-  end
-end
-
-describe service(service) do
-  it { should be_running }
-  it { should be_enabled }
-end
-
-ports.each do |p|
-  describe port(p) do
-    it { should be_listening }
-  end
+describe command(sasldblistusers_command) do
+  its(:stdout) { should match(/^foo@reallyenglish\.com: userPassword$/) }
+  its(:stderr) { should match(/^$/) }
+  its(:exit_status) { should eq 0 }
 end
